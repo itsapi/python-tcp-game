84b9a9b642ae3842147003668a81535a42fb15a3
Oliver Faircliff <mail@oliverfaircliff.com>
2015-01-03 17:07:01 +0000

    Assorted fixes

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ from threading import Thread
 class Server:
   def __init__(self):
     self.conn = socket.socket()
-    self.conn.bind(('0.0.0.0', 1337))
+    self.conn.bind(('0.0.0.0', 1339))
     self.conn.listen(5)
     
     self.clients = []
@@ -18,9 +18,15 @@ class Server:
       while True:
         pass
     except KeyboardInterrupt:
-      self.running = False
-      self.conn.shutdown(socket.SHUT_RDWR)
-      self.conn.close()
+      self.terminate()
+      
+  def terminate(self):
+    print('Terminating')
+    self.running = False
+    self.conn.shutdown(socket.SHUT_RDWR)
+    self.conn.close()
+    for client in self.clients:
+      self.term_client(client)
 
   def listen(self):
     while self.running:
@@ -32,25 +38,51 @@ class Server:
           raise err
 
   def handle(self, client, data):
+    print('Sending data')
     for other_client in self.clients:
       if other_client is not client:
-        other_client.conn.send(data)
         try:
-          pass
-        except:
-          self.clients.remove(other_client)
+          other_client.conn.send(data)
+        except socket.error:
+          self.term_client(other_client)
+          
+  def term_client(self, client):
+    try:
+      client.terminate()
+    except socket.error:
+      print('Exception during client termination')
+    finally:
+      print('Removing client from list')
+      self.clients.remove(client)
 
 
 class Client:
   def __init__(self, server, conn, addr):
+    print('New client connected')
     self.server = server
     self.conn = conn
     self.addr = addr
     
+    self.listener = Thread(target=self.listen)
+    self.listener.start()
+  
   def listen(self):
     while self.server.running:
-      data = self.conn.recv(1024)
-      self.server.handle(self, data)
+      try:
+        data = self.conn.recv(1024)
+        if len(data):
+          self.server.handle(self, data)
+        else:
+          self.terminate()
+          break
+      except socket.error as err:
+        if self.server.running:
+          raise err
+  
+  def terminate(self):
+    self.conn.shutdown(socket.SHUT_RDWR)
+    self.conn.close()
+    print('Terminated client')
 
 
 def main():
